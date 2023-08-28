10872f03ae570541401c96b6b77def1243c067c0
Daniel <dandcalvo@gmail.com>
2023-08-28 14:27:33 -0700

    add htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/scripts/main.js',
@@ -7,6 +8,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/scripts'),
         clean: true,
     },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Sim',
+        filename: '../index.html',
+        template: './dist/index_template.html'
+    })
+    ],
     resolve: {
         alias: {
             Script: path.resolve(__dirname, 'src/scripts'),
